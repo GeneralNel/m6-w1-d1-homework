85eec4599a40caf53a863e77fe7d05d38341f757
Mushfiq Rashid <mushfiqarashid@gmail.com>
2026-02-07 12:54:29 -0800

    setup server.js with working database

---
+++ node-mongo/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+require('./app/models/inventory.model.js');
+require('dotenv').config();
+const mongoose = require('mongoose');
+require('./app/routes/inventory.router.js')(app);
+
+mongoose.connect(process.env.DATABASE);
+
+mongoose.connection.on('error', err => {
+    console.log('MongoDB connection error: ' + err);
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('MongoDB connection open');
+});
+
+const server = app.listen(8080, function () {
+    const host = server.address().address;
+    const port = server.address().port;
+    console.log(`Server is running at http://${host}:${port}`);
+});
